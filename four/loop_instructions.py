9200b5bc71a06bc3cfc102ab94a516774e6dcd4d
uriel <urielieli@gmail.com>
2020-03-30 17:49:50 +0300

    added the four dir

---
+++ four/loop_instructions.py
@@ -0,0 +1,47 @@
+from formatting import indent_multiline
+from instructions import Instruction, RootInstruction
+
+
+class LoopInstruction(RootInstruction):
+	arity = 1
+	indent = 1
+	opcode = 8
+
+	def __init__(self, *params):
+		super().__init__(self.opcode, *params)
+
+	def __str__(self):
+		string = Instruction.__str__(self)
+
+		if self.instructions:
+			return '{}\n{}'.format(string,
+								   super().__str__())
+		return string
+
+	@property
+	def python(self):
+		transpiled = 'while {0} != 0:'.format(self.params[0].python)
+
+		if self.instructions:
+			return '{}\n{}'.format(transpiled,
+								   indent_multiline('\n'.join(map(lambda inst: inst.python, self.instructions)),
+										            self.indent))
+		return transpiled
+
+	@property
+	def cell(self):
+		return self[0]
+	@cell.setter
+	def cell(self, value):
+		self[0] = value
+
+class EndLoopInstruction(Instruction):
+	arity = 0
+	opcode = 9
+
+	def __init__(self, *params):
+		super().__init__(self.opcode, *params)
+
+	@property
+	def python(self):
+		return ''
